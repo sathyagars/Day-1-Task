31e59d2530645c31d1ac1ebb2d320b5136c7df0d
sathyagars <sathyagarsakthivel@gmail.com>
2024-05-28 17:33:20 +0530

    first commit

---
+++ task3.js
@@ -0,0 +1,39 @@
+
+
+let obj1 = {name:"person1", age:5};
+let obj2 = {age:5, name:"person1"};
+
+var assumption=true;
+
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(let key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            assumption=false;
+            break;
+        }
+    }
+}
+else {
+    assumption=false;
+}
+console.log(assumption);
+
+
+
+var request = new XMLHttpRequest();
+
+request.open("GET","https://restcountries.com/v3/all");
+
+request.send();
+
+request.onload=function(){
+    var result=JSON.parse(request.response);
+    for(let i = 0 ; i< result.length ; i++){
+        console.log(result[i].flags);   
+
+}
+}
+
